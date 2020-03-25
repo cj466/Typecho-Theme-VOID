e7fcfe5f17889b149c69e8897054cc0a5c26ddd6
cj466 <zs@fuoshuo.com>
2020-03-25 22:43:06 +0800

    🌈 隐藏控制台输出内容

--- assets/VOID.js
+++ assets/VOID.js
@@ -5,9 +5,9 @@
 // VOID
 // Author: 熊猫小A
 // Link: https://blog.imalan.cn/archives/247/
-
+/*
 console.log(' %c Theme VOID %c https://blog.imalan.cn/archives/247/ ', 'color: #fadfa3; background: #23b7e5; padding:5px;', 'background: #1c2b36; padding:5px;');
-
+*/
 var VOID_Content = {
     countWords: function () {
         if ($('#totalWordCount').length) {
